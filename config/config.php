e7f41cf0077d21221a0700e8c7dfb3803386a92a
HichamHamza786 <123880409+HichamHamza786@users.noreply.github.com>
2023-06-23 15:07:35 +0200

    Delete config directory

--- config/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Fichier de configuration
-define('API_URL', 'https://api.openweathermap.org/data/2.5/weather?q=Strasbourg,France&lang=fr&units=metric&appid=c0e6107d02ca25b9ca742a3713a5b662');
-?>
