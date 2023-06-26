54cf85e545fcdabbb80fdd3a498a3209e0172258
Jableur <hicham.hamza.attari@gmail.com>
2023-06-26 09:41:46 +0200

    Pattern MVC + city image displaying

---
+++ controllers/MainController.php
@@ -0,0 +1,81 @@
+<?php 
+
+class MainController {
+    public function getWeather($city) {
+        if (isset($_GET['city'])) {
+            $city = $_GET['city'];
+    
+            // Construction de l'URL des clés API
+            $apiWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" . urlencode($city) . "&appid=" . urlencode(WEATHER_API_KEY);
+            
+            // Requête API + obtention des données météo
+            $raw = file_get_contents($apiWeatherUrl);
+            $json = json_decode($raw);
+            
+            
+            // Vérification aboutissement requête météo
+            if ($json && $json->cod === 200) {
+                $name = $json->name;
+                //* Météo
+                $weather = $json->weather[0]->main;
+                $desc = $json->weather[0]->description;
+                //* Températures
+                $temp = $json->main->temp - 273.15;
+                $feel_like = $json->main->feels_like - 273.15;
+                //* Vent
+                $speed = $json->wind->speed;
+                $deg = $json->wind->deg;
+
+                $weatherData = [
+                    'name' => $name,
+                    'weather' => $weather,
+                    'desc' => $desc,
+                    'temp' => $temp,
+                    'feel_like' => $feel_like,
+                    'speed' => $speed,
+                    'deg' => $deg
+                ];
+
+                return $weatherData;
+            } else {
+                $name = "Ville non trouvée";
+                $weather = "";
+                $desc = "";
+                $temp = "";
+                $feel_like = "";
+                $speed = "";
+                $deg = "";
+            }
+        } else {
+            $name = "Erreur de connexion à l'API";
+            $weather = "";
+            $desc = "";
+            $temp = "";
+            $feel_like = "";
+            $speed = "";
+            $deg = "";
+        }
+        
+    }
+
+    public function getCityPhoto($city)
+    {
+        $city = $_GET['city'];
+        $apiPlacesUrl = "https://api.unsplash.com/photos/random?query=" . urlencode($city) . "&client_id=" . urlencode(PLACES_API_KEY);
+
+        $response = file_get_contents($apiPlacesUrl);
+        $data = json_decode($response, true);
+
+        // Vérifier si la réponse contient une image
+        if (isset($data['urls']['regular'])) {
+            $photoUrl = $data['urls']['regular'];
+
+            // Afficher la photo
+            echo "<img src='$photoUrl' alt='Photo de la ville'>";
+        } else {
+            echo 'Aucune photo disponible pour cette ville.';
+        }
+
+        return $photoUrl;
+    }
+}
--- index.php
+++ index.php
@@ -1,153 +1,107 @@
 <?php
     require_once('./config/config.php');
+    require_once __DIR__ . "/views/header.tpl.php";
+    require_once __DIR__ . "/controllers/MainController.php";
     
-    // Vérification API + input
-    if (isset($_GET['city'])) {
-        $city = $_GET['city'];
-
-        // Construction de l'URL de l'API
-        $apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" . urlencode($city) . "&appid=" . urlencode(API_KEY);
-        
-        // Requête API + obtention des données météo
-        $raw = file_get_contents($apiUrl);
-        $json = json_decode($raw);
-        
-        
-        // Vérification aboutissement requête
-        if ($json && $json->cod === 200) {
-            $name = $json->name;
-            //* Météo
-            $weather = $json->weather[0]->main;
-            $desc = $json->weather[0]->description;
-            //* Températures
-            $temp = $json->main->temp - 273.15;
-            $feel_like = $json->main->feels_like - 273.15;
-            //* Vent
-            $speed = $json->wind->speed;
-            $deg = $json->wind->deg;
-        } else {
-            $name = "Ville non trouvée";
-            $weather = "";
-            $desc = "";
-            $temp = "";
-            $feel_like = "";
-            $speed = "";
-            $deg = "";
-        }
-    } else {
-        $name = "Erreur de connexion à l'API";
-        $weather = "";
-        $desc = "";
-        $temp = "";
-        $feel_like = "";
-        $speed = "";
-        $deg = "";
-    }
-
-
-        
-    
+    $mainController = new MainController();
+    $weatherData = $mainController->getWeather($city);
 ?>
 
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!--Bootstrap-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css" integrity="sha512-t4GWSVZO1eC8BM339Xd7Uphw5s17a86tIZIj8qRxhnKub6WoyhnrxeCIMeAqBPgdZGlCcG2PrZjMc+Wr78+5Xg==" crossorigin="anonymous" referrerpolicy="no-referrer" />    
-    <!--Style-->
-    <link rel="stylesheet" href="./css/style.css">
-    <!--Favicon-->
-    <link rel="shortcut icon" href="/img/icons8-sun-cloud-16.png"/>
-    <link rel="apple-touch-icon" href="/img/icons8-sun-cloud-16.png"/>
-    <title>Weazer</title>
-</head>
-<body>
-    <div class="container text-center py-5">
-        <form action="" method="GET">
-            <input type="text" name="city" placeholder="Entrez une ville">
-            <button type="submit">Afficher la météo</button>
-        </form>
-        <h1>Météo du jour à <strong><?php echo $name;?></strong></h1>
+    <div class="container d-flex">
+        <div class="container text-center py-5">
+            <form action="" method="GET">
+                <input type="text" name="city" placeholder="Entrez une ville">
+                <button type="submit">Afficher la météo</button>
+            </form>
+            <h2>Météo du jour à <strong><?php echo $name;?></strong></h2>
 
-        <div class="row justify-content-center align-items-center">
-            <?php
-                switch($weather) {
-                    case "Clear":
-                    ?>
-                        <div class="icon sunny">
-                            <div class="sun">
-                                <div class="rays">
-                        </div>
+            <div class="row justify-content-center align-items-center">
+                <?php
+                    switch($weather) {
+                        case "Clear":
+                        ?>
+                            <div class="icon sunny">
+                                <div class="sun">
+                                    <div class="rays">
                             </div>
-                        </div>
-                    <?php 
-                    break;
-
-                    case 'Drizzle':
-                    ?>
-                    <div class="icon sun-shower">
-                        <div class="cloud">
-                            <div class="sun">
-                                <div class="rays"></div>
+                                </div>
                             </div>
-                        </div>
-                            <div class="rain"></div>
-                    </div>
-                    <?php 
-                    break;
+                        <?php 
+                        break;
 
-                    case 'Rain':
-                    ?>
-                        <div class="icon rainy">
+                        case 'Drizzle':
+                        ?>
+                        <div class="icon sun-shower">
+                            <div class="cloud">
+                                <div class="sun">
+                                    <div class="rays"></div>
+                                </div>
+                            </div>
+                                <div class="rain"></div>
+                        </div>
+                        <?php 
+                        break;
+
+                        case 'Rain':
+                        ?>
+                            <div class="icon rainy">
+                                <div class="cloud"></div>
+                                <div class="rain"></div>
+                            </div>
+                        <?php 
+                        break;
+
+                        case 'Clouds':
+                        ?>
+                        <div class="icon cloudy">
                             <div class="cloud"></div>
-                            <div class="rain"></div>
+                            <div class="cloud"></div>
                         </div>
-                    <?php 
-                    break;
+                        <?php
+                        break;
 
-                    case 'Clouds':
-                    ?>
-                    <div class="icon cloudy">
-                        <div class="cloud"></div>
-                        <div class="cloud"></div>
+                        case 'Thunderstorm':
+                        ?>
+                        <div class="icon thunder-storm">
+                            <div class="cloud"></div>
+                                <div class="lightning">
+                                    <div class="bolt"></div>
+                                    <div class="bolt"></div>
+                                </div>
+                        </div>
+                        <?php
+                        break;
+
+                        case 'Snow':
+                        ?>
+                        <div class="icon flurries">
+                            <div class="cloud"></div>
+                                <div class="snow">
+                                    <div class="flake"></div>
+                                    <div class="flake"></div>
+                                </div>
+                        </div>
+
+                        <?php
+                        break;
+                    }
+                ?>
+
+                <div class="meteo_desc text-left">
+                    <h2>
+                        <?php echo round($temp); ?> °C / Ressenti <?php echo round($feel_like); ?> °C <br />
+                        <?php echo round($speed); ?> Km/h <br />
+                    </h2>
+                </div>
+            </div>
+        </div>
+        <div class="container w-50">
+            <div id="carouselExample" class="carousel slide">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="<?= $mainController->getCityPhoto($city);?>" class="d-block w-100" alt="Photos de la ville">
                     </div>
-                    <?php
-                    break;
-
-                    case 'Thunderstorm':
-                    ?>
-                    <div class="icon thunder-storm">
-                        <div class="cloud"></div>
-                            <div class="lightning">
-                                <div class="bolt"></div>
-                                <div class="bolt"></div>
-                            </div>
-                    </div>
-                    <?php
-                    break;
-
-                    case 'Snow':
-                    ?>
-                    <div class="icon flurries">
-                        <div class="cloud"></div>
-                            <div class="snow">
-                                <div class="flake"></div>
-                                <div class="flake"></div>
-                            </div>
-                    </div>
-
-                    <?php
-                    break;
-                }
-            ?>
-
-            <div class="meteo_desc text-left">
-                <h2>
-                    <?php echo round($temp); ?> °C / Ressenti <?php echo round($feel_like); ?> °C <br />
-                    <?php echo round($speed); ?> Km/h <br />
-                </h2>
+                </div>
             </div>
         </div>
     </div>
